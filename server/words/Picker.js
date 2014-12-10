28925257cc0338e9d2ed1c357b43a1c178c52bdb
Joe Coulson <joecoulson@gmail.com>
2014-12-10 21:05:23 +0000

    Finishing off next letter logic and tests

--- server/words/Picker.js
+++ server/words/Picker.js
@@ -1,34 +1,107 @@
 var fs = require('fs');
+var EventEmitter = require('events').EventEmitter;
+var utils = require('util');
 
-(function(){
-
-	var words = [];
+(function() {
+	var words = {};
 	var wordsLoaded = false;
 
-	function Picker(){
+	function Picker(path) {
+		var o = this;
 
-	}
+		fs.readFile(path || './server/words/list.json', 'utf8', function(err, list) {
+			if (err) {
+				return o.emit('loaderror', err);
+			}
 
-	fs.readFile('./server/words/list.json', 'utf8', function(err, list){
-		var error = function(err){
-			console.error('Couldn\'t read words JSON from ' + process.env.PWD, err);
-		};
-
-		if(err) {	
-			return error(err);
-		}
-
-		try {
-			words = JSON.parse(list);
+			o.words = words = JSON.parse(list);
 			wordsLoaded = true;
-		} catch(e) {
-			return error(e);
-		}
-	});
+			o.emit('ready');
 
-	Picker.prototype.nextLetter = function(){
-		return '';
-	}
+		});
+	};
+
+	utils.inherits(Picker, EventEmitter);
+
+	/**
+	 * @method nextLetter
+	 * @param str {string}
+	 * @return {string}
+	 * Fetches a suitable word and returns the next letter
+	 */
+	Picker.prototype.nextLetter = function(str) {
+		if(!str) {
+			return
+		}
+		var options = this.fetchMatches(str);
+		var returned = {};
+		var rand = Math.floor(Math.random() * (options && options._p && options._p.length || 0)); // _p are preferred words (i.e. ones for the computer to win);
+		var nextWord, nextLetter;
+
+
+		if (words._list.indexOf(str) >= 0) {
+			returned.playerLoses = true;
+			return;
+		}
+
+		if (options) {
+			if (options._p.length) {
+				nextWord = words._list[options._p[rand][0]];
+			} else {
+				nextWord = getRandomMaxLength(options._w); // _w are words for the computer to lose
+			}
+		}
+
+		return {
+			nextLetter: nextWord ? nextWord[str.length] : ''
+		};
+	};
+
+	/**
+	 * @method fetchMatches
+	 * @param str {string}
+	 * @return {Object}
+	 * Descends in to the tree and returns a matching object or undefined if there is no match
+	 */
+
+	Picker.prototype.fetchMatches = function(str) {
+		var descend = function(i, branch) {
+			if (i === str.length || !branch) {
+				return branch;
+			}
+			return descend(i + 1, branch[str[i]]);
+		}
+
+		if (str) {
+			return descend(0, words);
+		}
+	};
+
+	/**
+	 * @function getRandMaxLength
+	 * @param arr {Array} possible word matches
+	 * @private
+	 * @return {number}
+	 * Instead of just picking the last array item (and therefore longest)
+	 * this will select randomly from the longest words
+	 */
+	var getRandomMaxLength = function(arr){
+		var range;
+		var rands = [];
+		var max = 0;
+		var i = arr.length;
+
+		while(i--) {
+			if(max<arr[i][1]) {
+				break;
+			}
+			rands.push(arr[i][0]);
+			max = arr[i][1];
+		}
+
+		// set range here and then return the random max length word.
+		return rands[Math.floor(Math.random() * (rands.length || 0))];
+	};
 
 	module.exports = Picker;
-}())
+}());
--- test/Picker.spec.js
+++ test/Picker.spec.js
@@ -1,10 +1,46 @@
-var name = __filename.split('/')[__filename.split('/').length-1].split('.')[0];
-Picker = require('../server/words/Picker.js');
+var fs = require('fs');
+var path = require('path');
+var Picker = require('../server/words/Picker.js');
 
-console.log(typeof process[name]);
 describe('Picker', function() {
+	var words = JSON.parse(fs.readFileSync('./server/words/list.json'));
+
 	it('should exist', function() {
 		Picker.should.be.a.Function;
+	});
+
+	it('should create an event emitter', function(){
+		var picker = new Picker();
+		picker.on.should.be.a.Function;
+		picker.removeListener.should.be.a.Function;
+		picker.emit.should.be.a.Function;
+	});
+
+
+	it('should emit a ready event when the list.json file is loaded', function(done){
+		var picker = new Picker();
+		picker.on('ready', function(){
+			picker.should.be.ok;
+			done();
+		});
+	});
+
+	it('should emit a loaderror event if the file load in the constructor errors', function(done){
+		var p = new Picker('fail.json');
+		p.on('loaderror', function(e){
+			e.should.be.ok;
+			done();
+		});
+	});
+
+	it('should be able to load in any JSON encoded file to the words property', function(done){
+		var p = new Picker(path.resolve(__dirname, 'files/test.json'));
+		p.on('ready', function(){
+			done();
+		});
+		p.on('loaderror', function(e){
+			throw new Error(e);
+		})
 	});
 
 	describe('nextLetter', function(){
@@ -13,9 +49,55 @@ describe('Picker', function() {
 			picker.nextLetter.should.be.a.Function;
 		});
 
-		it('should return a string', function(){
-			var picker = new Picker();
-			picker.nextLetter().should.be.a.String
+		it('should return undefined if no string is passed', function(done){
+			var p = new Picker(path.resolve(__dirname, 'files/list.json'));
+			p.on('ready', function(){
+				(p.nextLetter() === undefined).should.be.ok;
+				done();
+			});
 		});
-	})
+
+		it('should only return an letter if a string is passed to it', function(done){
+			var p = new Picker(path.resolve(__dirname, 'files/list.json'));
+			p.on('ready', function(){
+				p.nextLetter('za').should.be.ok;
+				done();
+			});
+
+			p.on('loaderror', function(e){
+				throw new Error(e);
+			})
+		});
+	});
+
+	describe('fetchMatches', function(){
+		it('should exist', function(){
+			var p = new Picker();
+			p.fetchMatches.should.be.a.Function;
+		});
+
+		it('should return an object', function(done){
+			var p = new Picker();
+			p.on('ready', function(){
+				p.fetchMatches('a').should.be.a.Object;
+				done();
+			});
+		});
+
+		it('should return the object that matches the position in the tree according to the string passed to it', function(done){
+			var list = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'files/list.json')));
+			var p = new Picker(path.resolve(__dirname, 'files/list.json'));
+			p.on('ready', function(){
+				var res = p.fetchMatches('zan')._w;
+				for(var i = res.length-1; i >= 0; i--) {
+					res[i][0].should.equal(list.z.a.n._w[i][0]);
+				}
+				done();
+			});
+
+			p.on('loaderror', function(e){
+				throw new Error(e);
+			})
+		});
+	});
 });
