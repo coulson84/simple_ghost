65cff5ca009b31905a1c8e81c51544035639ec25
Joe Coulson <joecoulson@gmail.com>
2014-12-10 21:16:10 +0000

    Committing changes to process words to formulate trie correctly

--- resources/grunt/processWords.js
+++ resources/grunt/processWords.js
@@ -1,34 +1,71 @@
 module.exports = function(grunt) {
 	grunt.registerMultiTask('processWords', function() {
-		var wordsList = grunt.file.read(this.data.src).split('\n');
+		var o = this;
+		var start = new Date();
+		var shorterMatch = false;
+		console.log("Processing word.lst file");
+		console.log(start.toLocaleTimeString() + ' ' + start.toLocaleDateString());
+		var wordList = grunt.file.read(this.data.src).split('\n');
 		var tree = {
-			words: wordsList
+			_list: [],
+			_w: [],
+			_p: [],
+			_a: []
 		};
-		var i = wordsList.length - 1; // words.lst finishes with an new line break so has a an empty array item at the end we don't want
+		var waste = {};
+		var i = wordList.length - 1; // w.lst finishes with an new line break so has a an empty array item at the end we don't want
 		var j, m;
-		var map = function(branch, word, wordIndex, letterIndex) {
-			var newBranch;
-			if(letterIndex < word.length) {
+		var map = function(branch, word, wordIndex, letterIndex, parent) {
+			var sliced = word.slice(0, letterIndex);
+			var newBranch, i;
+			if (letterIndex < word.length) {
 				newBranch = branch[word[letterIndex]] = branch[word[letterIndex]] || {
-					words: [],
-					preferred: []
+					_w: [],
+					_p: []//,
+					//_a: []
 				};
 
-				if(word.length%2===1) {
-					newBranch.preferred.push(wordIndex);
+				if (word.length % 2 === 1) {
+					// _p are preferred words (for the computer) that will result in a win for the computer
+					newBranch._p.push([wordIndex, word.length]);
 				} else {
-					newBranch.words.push(wordIndex);
+					// _w are words that will result in a loss for the computer
+					newBranch._w.push([wordIndex, word.length]);
 				}
-				map(newBranch, word, wordIndex, ++letterIndex);
+				map(newBranch, word, wordIndex, ++letterIndex, branch);
 			}
 		};
 
-		while (i--) {
-			if(wordsList[i].length >= 4) {
+		if(this.target === 'test') {
+			wordList = wordList.slice(1, 500).concat(wordList.slice(-500));
+			i = wordList.length
+		}
 
-				map(tree, wordsList[i], i, 0);
+		wordList = wordList.sort(function(a, b) {
+			return b.length - a.length;
+		});
+
+		while (i--) {
+			shorterMatch = false;
+			word = wordList[i]
+
+			if (word.length >= 4) {
+				// all parsed words are added to the waste object - if a shorter word exists as a 
+				// substring of the current string then we don't need to process the word at all
+				// originally checked this via an array, using indexOf but this was way faster
+				for (j = 4, m = word.length; j < word.length; j++) {
+					if (waste[word.slice(0, j)]) {
+						shorterMatch = true;
+					}
+				}
+				if (shorterMatch === false) {
+					waste[word] = true;
+					map(tree, word, tree._list.push(word) - 1, 0);
+				} {}
 			}
 		}
+
+		console.log("Processing complete - time taken: " + ((new Date().getTime() - start) / 1000).toFixed(2) + 's');
 		grunt.file.write(this.data.dest, JSON.stringify(tree));
 	});
 };
